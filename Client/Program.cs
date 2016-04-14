3b336a875c605321536b76e5961673504b326970
Sbreso <sbresograncha@gmail.com>
2016-04-14 13:09:21 +0200

    Client envia y recibe sms´s

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Client
@@ -10,6 +12,21 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            //Thread.Sleep(2500);
+            IHubProxy hub;
+            string url = "http://localhost:8080/signalr";
+            HubConnection connection = new HubConnection(url);
+
+            hub = connection.CreateHubProxy("MyHub");
+            connection.Start().Wait();
+            hub.On("addMessage", x => Console.WriteLine(x));
+
+            //ConnectAsync();
+            string line = "";
+            while ((line = Console.ReadLine()) != null)
+            {
+                hub.Invoke("Send", "Voro", line).Wait();
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 using System.Threading.Tasks;
 
 namespace Server
@@ -10,6 +12,39 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            using (WebApp.Start<Startup>("http://localhost:8080/"))
+            {
+                Console.WriteLine("Server running at http://localhost:8080/");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    [HubName("MyHub")]
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            String sms = String.Format("Message send by {0}: {1}", name, message);
+            Console.WriteLine(sms);
+            Clients.All.addMessage(sms);
+        }
+        public override Task OnConnected()
+        {
+            Console.WriteLine("Client connected: " + Context.ConnectionId);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(Boolean stopCall)
+        {
+            Console.WriteLine("Client disconnected: " + Context.ConnectionId);
+            return base.OnDisconnected(true);
         }
     }
 }
